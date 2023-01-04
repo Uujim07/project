f1a1e7026b5489e8572aa8484159d8c5dd227aac
Uujim Gombo <uujim@Uujims-MacBook-Air.local>
2023-01-04 18:56:16 +0800

    bolson

--- project-type/front-end/src/comp/Home.jsx
+++ project-type/front-end/src/comp/Home.jsx
@@ -1,127 +1,133 @@
-import React from 'react'
-import img from './logo-default.png'
-import {useState}  from 'react';
-const styles={
-    container:{
-        display:"flex",
-        justifyContent:"end",
-        paddingRight:"40px"
-    
-    },
-    button1:{
-        color:"#02B589",
-        fontFamily: 'Ubuntu',
-        fontStyle: "normal",
-        fontSize: 20,
-        lineHeight: 3,
-        textTransform: "uppercase",
-        paddingRight:"40px",
-        border: "none",
-        backgroundColor:"white"
-        
-    },
-    button2:{
-        marginTop:"10px",
-        width: 183,
-        height: 44,
-        borderRadius:"20px 20px 20px 20px",
-        backgroundColor:"#02B589",
-        border: "none",
-        
-        
-    },
-    letter:{
-        color:"white",
-        fontFamily: 'Ubuntu',
-        fontStyle: "normal",
-        fontSize: 20,
-        textTransform: "uppercase",
-    },
-    main:{
-        display:"flex",
-        justifyContent:"center",
-        paddingTop:"230px",
-            
-        
-    },
-    logo:{
-        width:184,
-        height:118,
-        
-    }, search:{
-        width: 581,
-        height: 44,
-        borderRadius:"20px 20px 20px 20px",
-        
-    },
-    holder:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        height:"20vh"
-
-    },
-    button3:{
-        width: 192,
-        height: 44,
-        color:"white",
-        fontFamily: 'Ubuntu',
-        fontStyle: "normal",
-        fontSize: 20,
-        textTransform: "uppercase",
-        backgroundColor:"#02B589",
-        borderRadius:"20px 20px 20px 20px",
-        marginLeft:"20px",
-        border: "none",
-    },
-    footer:{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '50vh',
-
-        
-    }, id:{
-        display:"block"
-        
-    }, did:{
-        color:"grey",
-        paddingLeft:"40px"
-    }
-
-}
- const Home = () => {
-    const [message, setMessage] = useState('');
-    const handleChange =(event)=>{
-         setMessage(event.target.value);
-         console.log(event.target.value)
-    }
+import React from "react";
+import img from "./logo-default.png";
+import { useState } from "react";
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "end",
+    paddingRight: "40px",
+  },
+  button1: {
+    color: "#02B589",
+    fontFamily: "Ubuntu",
+    fontStyle: "normal",
+    fontSize: 20,
+    lineHeight: 3,
+    textTransform: "uppercase",
+    paddingRight: "40px",
+    border: "none",
+    backgroundColor: "white",
+  },
+  button2: {
+    marginTop: "10px",
+    width: 183,
+    height: 44,
+    borderRadius: "20px 20px 20px 20px",
+    backgroundColor: "#02B589",
+    border: "none",
+  },
+  letter: {
+    color: "white",
+    fontFamily: "Ubuntu",
+    fontStyle: "normal",
+    fontSize: 20,
+    textTransform: "uppercase",
+  },
+  main: {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "230px",
+  },
+  logo: {
+    width: 184,
+    height: 118,
+  },
+  search: {
+    width: 581,
+    height: 44,
+    borderRadius: "20px 20px 20px 20px",
+  },
+  holder: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "20vh",
+  },
+  button3: {
+    width: 192,
+    height: 44,
+    color: "white",
+    fontFamily: "Ubuntu",
+    fontStyle: "normal",
+    fontSize: 20,
+    textTransform: "uppercase",
+    backgroundColor: "#02B589",
+    borderRadius: "20px 20px 20px 20px",
+    marginLeft: "20px",
+    border: "none",
+  },
+  footer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "50vh",
+  },
+  id: {
+    display: "block",
+  },
+  did: {
+    color: "grey",
+    paddingLeft: "40px",
+  },
+  url: {
+    display: "block",
+    marginLeft: "350px",
+    height: "10vh",
+  },
+};
+const Home = () => {
+  const [message, setMessage] = useState("");
+  const [updated, setUpdated] = useState(message);
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+    console.log(event.target.value);
+  };
+  const copy = () => {
+    setUpdated(message);
+  };
   return (
     <div>
-        <div  style ={styles.container}>
-            <button style={styles.button1}> Хэрхэн ажилладаж вэ?</button>
-        <button style={styles.button2}> 
-            <div style={styles.letter}>Нэвтрэх</div>
+      <div style={styles.container}>
+        <button style={styles.button1}> Хэрхэн ажилладаж вэ?</button>
+        <button style={styles.button2}>
+          <div style={styles.letter}>Нэвтрэх</div>
         </button>
-       </div>
-       <div style={styles.main}>
-        <img src={img} style={styles.logo} ></img>
-       </div>
-       <div style={styles.holder}>
-            <input id="message"  onChange={handleChange}style={styles.search} placeholder="https://www.web-huudas.mn"></input>
-            <button  style={styles.button3}>Богиносгох</button>
-            
-       </div>
-       <div>Message: {message}</div>
-       <div style={styles.footer}>
-                <div style={styles.id}>
-                    <div>Made with ♥️ by Nest Academy</div>
-                    <div style={styles.did}>©boginoo.io 2020</div>
-                </div>
-                
-            </div>
+      </div>
+      <div style={styles.main}>
+        <img src={img} style={styles.logo}></img>
+      </div>
+      <div style={styles.holder}>
+        <input
+          id="message"
+          onChange={handleChange}
+          style={styles.search}
+          placeholder="https://www.web-huudas.mn"
+        ></input>
+        <button onClick={copy} style={styles.button3}>
+          Богиносгох
+        </button>
+      </div>
+      <div style={styles.url}>
+        <div>Өгөгдсөн холбоос:</div>
+        <div>Message: {updated}</div>
+      </div>
+      <div style={styles.footer}>
+        <div style={styles.id}>
+          <div>Made with ♥️ by Nest Academy</div>
+          <div style={styles.did}>©boginoo.io 2020</div>
+        </div>
+      </div>
     </div>
-    
-  )
-}
-export default Home
+  );
+};
+export default Home;
--- project-type/front-end/src/comp/login.jsx
+++ project-type/front-end/src/comp/login.jsx
@@ -1,71 +1,126 @@
-import React from 'react'
-import img from './logo-default.png'
-const styles={
-    header:{
-        
-        color:"#02B589",
-        fontFamily: 'Ubuntu',
-        fontStyle: "normal",
-        fontSize: 20,
-        lineHeight: 3,
-        textTransform: "uppercase",
-        border: "none",
-        backgroundColor:"white",
-        paddingRight:"40px"
-    },
-    back:{
-        width:"100vw",
-        display:"flex",
-        justifyContent:"end",
-    },
-    image:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        height:"60vh"
-    }, egnee:{
-        display: "block",
-        margin:"auto",
-    },
-    nevter:{
-        fontFamily: 'Ubuntu',
-        fontStyle: "normal",
-        fontSize: 32,
-        textTransform: "uppercase",
-        color:"#02B589",
-        paddingLeft:"20px",
-        paddingTop:"60px"
-    },
-    bar1:{
-        width: 381,
-        height: 44,
-        borderRadius:"20px 20px 20px 20px",
-        marginRight:"150px"
-    }
-}
- const login = () => {
+import React from "react";
+import img from "./logo-default.png";
+const styles = {
+  header: {
+    color: "#02B589",
+    fontFamily: "Ubuntu",
+    fontStyle: "normal",
+    fontSize: 20,
+    lineHeight: 3,
+    textTransform: "uppercase",
+    border: "none",
+    backgroundColor: "white",
+    paddingRight: "40px",
+  },
+  back: {
+    width: "100vw",
+    display: "flex",
+    justifyContent: "end",
+  },
+  image: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "60vh",
+  },
+  egnee: {
+    display: "block",
+    margin: "auto",
+  },
+  nevter: {
+    fontFamily: "Ubuntu",
+    fontStyle: "normal",
+    fontSize: 32,
+    textTransform: "uppercase",
+    color: "#02B589",
+    paddingLeft: "20px",
+    paddingTop: "60px",
+    paddingBottom: "50px",
+  },
+  bar1: {
+    width: 381,
+    height: 44,
+    borderRadius: "20px 20px 20px 20px",
+    marginRight: "150px",
+    marginTop: "20px",
+    marginBottom: "20px",
+  },
+  line: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginRight: "150px",
+  },
+  button2: {
+    backgroundColor: "white",
+    border: "none",
+    textDecoration: "underline",
+  },
+  nevtreh: {
+    width: "383px",
+    height: "43px",
+    backgroundColor: "#02B589",
+    borderRadius: "20px 20px 20px 20px",
+    fontFamily: "Ubuntu",
+    fontStyle: "normal",
+    fontSize: 20,
+    textTransform: "uppercase",
+    color: "white",
+    marginTop: "80px",
+    display: "block",
+  },
+  nevtreh2: {
+    border: "none",
+    backgroundColor: "white",
+    marginLeft: "80px",
+    color: "#02B589",
+    textDecoration: "underline",
+  },
+  footer: {
+    marginLeft: "80px",
+  },
+  did: {
+    paddingTop: "30px",
+    display: "block",
+  },
+};
+const login = () => {
   return (
     <div>
-        <div style={styles.back}>
-            <button style={styles.header}>Хэрхэн ажилладаж вэ?</button>
-        </div>
-        <div style={styles.image}>
-            <div style={styles.egnee}>
-                <img src={img}></img>
-                <div style={styles.nevter}>Нэвтрэх</div>
-                <div>
-                    <div>Цахим хаяг</div>
-                    <input style={styles.bar1} placeholder="name@mail.domain"></input>
-                </div>  
-                <div>
-                    <div>Нууц үг</div>
-                    <input style={styles.bar1} placeholder="••••••••••"></input>
-                </div>  
+      <div style={styles.back}>
+        <button style={styles.header}>Хэрхэн ажилладаж вэ?</button>
+      </div>
+      <div style={styles.image}>
+        <div style={styles.egnee}>
+          <img src={img}></img>
+          <div style={styles.nevter}>Нэвтрэх</div>
+          <div>
+            <div>Цахим хаяг</div>
+            <input style={styles.bar1} placeholder="name@mail.domain"></input>
+          </div>
+          <div>
+            <div>Нууц үг</div>
+            <input style={styles.bar1} placeholder="••••••••••"></input>
+          </div>
+          <div style={styles.line}>
+            <div>
+              <input type="checkbox"></input>
+              <label>Намайг сана</label>
             </div>
-              
+            <button style={styles.button2}>Нууц үгээ мартсан</button>
+          </div>
+          <button style={styles.nevtreh}>Нэвтрэх</button>
+          <button style={styles.nevtreh2}>
+            Шинэ хэрэглэгч бол энд дарна уу?
+          </button>
+          <div style={styles.footer}>
+            <div style={styles.did}>
+              <div>Made with ♥️ by Nest Academy</div>
+              <div>©boginoo.io 2020</div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 export default login;
